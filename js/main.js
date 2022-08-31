bdfebbdf683e7d35cc51b7a62f2db9b5f95aa0dc
AmdjedSanero <amdjed.sanero@gmail.com>
2022-08-31 09:54:03 +0100

    without btn minus and plus

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ let list = document.querySelector(".head .container .list");
 let close_bar = document.querySelector(
   ".head .container .logo-and-list .close-bar"
 );
-
+let thumbs = document.querySelectorAll(
+  ".context .container .gallery .thumbs img"
+);
+let image_product = document.querySelector(
+  ".context .container .gallery .image-product img"
+);
 three_bar.addEventListener("click", function () {
   list.classList.remove("not-opened");
   list.classList.add("opened");
@@ -16,3 +21,14 @@ close_bar.addEventListener("click", function () {
   list.classList.add("not-opened");
   close_bar.classList.add("none");
 });
+
+thumbs.forEach((img) =>
+  img.addEventListener("click", function () {
+    for (let i = 0; i < thumbs.length; i++) {
+      thumbs[i].classList.remove("active");
+    }
+    img.classList.add("active");
+    let numOfThumb = img.dataset.num;
+    image_product.src = `images/image-product-${numOfThumb}.jpg`;
+  })
+);
